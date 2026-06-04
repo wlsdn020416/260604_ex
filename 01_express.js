0ac6399d54b83be98c5cf0374ffe37ce93473ba8
wlsdn020416 <wlsdn020416@gmail.com>
2026-06-04 11:27:38 +0900

    Add initial project files and update .gitignore
    - Added .env to .gitignore
    - Implemented basic Express server with JSON parsing
    - Enhanced POST response in /chat route
    - Created resources.yaml for Postman workspace
    - Added empty SDK file
    - Created workspace.globals.yaml for Postman globals

--- 01_express.js
+++ 01_express.js
@@ -3,13 +3,22 @@ const express = require('express');
 const app = express();
 const PORT = 3333;
 
+app.use(express.json())
+
 app.get("/",(req, res) => {
     res.send("Hello Jinwoo");
 });
 
-app.post("/",(req,res) => {
+app.post("/", (req, res) => {
+  res.json({
+    msg: "어머 POST도 가능하네요",
+  });
+});
+
+app.post("/chat",(req,res) => {
+    const { msg } = req.body;
     res.json({
-        msg: "post",
+        reply: `${msg}랍니다.`,
     });
 });
 
